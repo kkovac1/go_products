58ca55cf728982dc54ddc7ab40b6dea7d9e35af6
Krunoslav Kovac <krunoslav.k@eeam.eu>
2025-03-30 21:15:27 +0200

    added create test for products

---
+++ service/products/routes_test.go
@@ -0,0 +1,57 @@
+package products
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/kkovac1/products/types"
+)
+
+type mockProductsStore struct{}
+
+func TestProductsServiceHandlers(t *testing.T) {
+	productsStore := &mockProductsStore{}
+	handler := NewHandler(productsStore)
+
+	t.Run(("Should create product"), func(t *testing.T) {
+		payload := types.CreateProductPayload{
+			Name:        "Test Product",
+			Description: "Test Description",
+			Price:       10.0,
+			Quantity:    5,
+		}
+
+		marshalled, _ := json.Marshal(payload)
+		req, err := http.NewRequest(http.MethodPost, "/products", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+
+		router.HandleFunc("/products", handler.handleCreateProduct)
+
+		router.ServeHTTP(rr, req)
+
+		if condition := rr.Code != http.StatusCreated; condition {
+			t.Errorf("Expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
+}
+
+func (m *mockProductsStore) GetProductById(productId int) (*types.Product, error) {
+	return &types.Product{}, nil
+}
+
+func (m *mockProductsStore) GetAllProducts() ([]*types.Product, error) {
+	return []*types.Product{}, nil
+}
+
+func (m *mockProductsStore) CreateProduct(product types.Product) error {
+	return nil
+}
